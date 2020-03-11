461a4b4040b5841eb2887af84ac30019efbb589d
wwf <865475679@qq.com>
2020-03-11 11:59:55 +0800

    添加样式

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,17 @@
 <template>
   <div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body{
-  line-height: 1.5;
-}
-a{
-  text-decoration: none;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "~@/assets/style/helper.scss";
+@import "~@/assets/style/reset.scss";
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: #333;
+  font-family: $font-hei;
+  line-height: 1.5;
 }
 </style>
