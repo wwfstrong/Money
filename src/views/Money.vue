f84c9f6e05e8d4d1d32db2e51a9aeb2f58f0e66c
wwf <865475679@qq.com>
2020-03-12 18:01:04 +0800

    修改

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,31 +1,54 @@
 <template>
   <Layuot class-prefix="layout">
-    <NumberPad />
-    <Types />
-    <Notes />
-    <Tags :data-source.sync="tags" />
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <Types :value.sync="record.type" />
+    <Notes @update:value="onUpdateNotes" />
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layuot>
 </template>
 
-<script lang="js">
+<script lang="ts">
+import Vue from "vue";
 import NumberPad from "@/components/money/NumberPad.vue";
 import Types from "@/components/money/Types.vue";
 import Notes from "@/components/money/Notes.vue";
 import Tags from "@/components/money/Tags.vue";
-export default {
-  name: "Money",
-  components: {
-    Tags,
-    Notes,
-    Types,
-    NumberPad
-  },
-  data(){
-    return {
-      tags:['衣','食','住','行']
-    }
-  }
+import { Component, Watch } from "vue-property-decorator";
+
+const recordList: Record[] = JSON.parse(
+  window.localStorage.getItem("recordList") || "[]"
+);
+type Record = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number; // 数据类型 object | string
+  createdAt?: Date; // 类 / 构造函数
 };
+
+@Component({
+  components: { Tags, Notes, Types, NumberPad }
+})
+export default class Money extends Vue {
+  tags = ["衣", "食", "住", "行"];
+  recordList: Record[] = recordList;
+  record: Record = { tags: [], notes: "", type: "-", amount: 0 };
+  onUpdateTags(value: string[]) {
+    this.record.tags = value;
+  }
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
+  saveRecord() {
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
+    this.recordList.push(record2);
+  }
+  @Watch("recordList")
+  onRecordListChange() {
+    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+  }
+}
 </script>
 <style lang="scss">
 .layout-content {
