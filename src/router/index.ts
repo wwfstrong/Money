f44bdf1f06a1ef22504aeed3f7a0a8e9d2adf483
wwf <865475679@qq.com>
2020-03-13 14:12:07 +0800

    创建标签

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
 import NotFound from '@/views/NotFound.vue'
+import EditLabel from '@/views/EditLabel.vue'
 
 Vue.use(VueRouter)
 
@@ -26,8 +27,12 @@ const routes = [
     component: Statistics
   },
   {
-    path:"*",
-    component:NotFound
+    path: "/labels/edit",
+    component: EditLabel
+  },
+  {
+    path: "*",
+    component: NotFound
   }
 ]
 
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layuot>
+    <div>编辑标签</div>
+  </Layuot>
+</template>
+
+<script lang='ts'>
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class EditLabel extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
