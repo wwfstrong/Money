b5bd1308e80111126fd20ef7926ccab0a6c374bd
wwf <865475679@qq.com>
2020-03-14 09:45:38 +0800

    封装 record 的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,15 +15,13 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
-import { Component, Watch } from "vue-property-decorator";
-import recordListModel from "@/models/recordListModel";
-const recordList = recordListModel.fetch();
+import { Component } from "vue-property-decorator";
 @Component({
   components: { Tags, FormItem, Types, NumberPad }
 })
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList= window.recordList;
   record: RecordItem = {
     tags: [],
     notes: "",
@@ -37,11 +35,7 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    recordListModel.create(this.record);
-  }
-  @Watch("recordList")
-  onRecordListChange() {
-    recordListModel.save();
+    window.createRecord(this.record);
   }
 }
 </script>
