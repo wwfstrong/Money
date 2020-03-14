7179125b788fee96f4bffeb86aa825ea78c62f06
wwf <865475679@qq.com>
2020-03-14 08:38:01 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,61 +1,59 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <Types :value.sync="record.type" />
     <div class="notes">
-      <FormItem field-name="备注"
-                placeholder="在这里输入备注"
-                @update:value="onUpdateNotes"
-      />
+      <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes" />
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
-  import FormItem from '@/components/Money/FormItem.vue';
-  import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
-  import tagListModel from '@/models/tagListModel';
-  const recordList = recordListModel.fetch();
-  const tagList = tagListModel.fetch();
-  @Component({
-    components: {Tags, FormItem, Types, NumberPad}
-  })
-  export default class Money extends Vue {
-    tags = tagList;
-    recordList: RecordItem[] = recordList;
-    record: RecordItem = {
-      tags: [], notes: '', type: '-', amount: 0
-    };
-    onUpdateTags(value: string[]) {
-      this.record.tags = value;
-    }
-    onUpdateNotes(value: string) {
-      this.record.notes = value;
-    }
-    saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
-    }
-    @Watch('recordList')
-    onRecordListChange() {
-      recordListModel.save(this.recordList);
-    }
+import Vue from "vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import FormItem from "@/components/Money/FormItem.vue";
+import Tags from "@/components/Money/Tags.vue";
+import { Component, Watch } from "vue-property-decorator";
+import recordListModel from "@/models/recordListModel";
+import tagListModel from "@/models/tagListModel";
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
+@Component({
+  components: { Tags, FormItem, Types, NumberPad }
+})
+export default class Money extends Vue {
+  tags = tagList;
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0
+  };
+  onUpdateTags(value: string[]) {
+    this.record.tags = value;
   }
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
+  saveRecord() {
+    recordListModel.create(this.record);
+  }
+  @Watch("recordList")
+  onRecordListChange() {
+    recordListModel.save();
+  }
+}
 </script>
 
 <style lang="scss">
-  .layout-content {
-    display: flex;
-    flex-direction: column-reverse;
-  }
-  .notes {
-    padding: 12px 0;
-  }
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+.notes {
+  padding: 12px 0;
+}
 </style>
