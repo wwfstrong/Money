e5cb2d3ffd54258b16927232c4b55c1c4c09ee63
wwf <865475679@qq.com>
2020-03-16 12:23:51 +0800

    完成列表的 CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -7,20 +7,62 @@
       <br />
       interval:{{interval}}
     </div>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import Tabs from "@/components/Tabs";
-import intervalList from "@/constants/intervalList";
-import recordTypeList from "@/constants/recordTypeList";
+import Tabs from "@/components/Tabs.vue";
+import intervalList from "@/constants/intervalList.ts";
+import recordTypeList from "@/constants/recordTypeList.ts";
 
 @Component({
   components: { Tabs }
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? "无" : tags.join(",");
+  }
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+  get result() {
+    const { recordList } = this;
+    type HashTableValue = { title: string; items: RecordItem[] };
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date] = recordList[i].createdAt!.split("T");
+      hashTable[date] = hashTable[date] || { title: date, items: [] };
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+  beforeCreate() {
+    this.$store.commit("fetchRecords");
+  }
   type = "-";
   interval = "day";
   intervalList = intervalList;
@@ -39,6 +81,27 @@ export default class Statistics extends Vue {
       }
     }
   }
+  .interval-tabs-item {
+    height: 48px;
+  }
 }
-
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title {
+  @extend %item;
+}
+.record {
+  background: white;
+  @extend %item;
+}
+.notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
 module.exports = {
@@ -13,7 +12,6 @@ module.exports = {
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false }).end()
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({ ...options, plugins: [{ removeAttrs: { attrs: 'fill' } }] })).end()
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }])
     config.module.rule('svg').exclude.add(dir)
   }
