24ef6f057fdfa706521131611cd216f4724b04a4
wwf <865475679@qq.com>
2020-03-21 12:44:00 +0800

    添加二维码

--- src/main.ts
+++ src/main.ts
@@ -21,14 +21,14 @@ new Vue({
   render: h => h(App)
 }).$mount('#app');
 
-if(document.documentElement.clientWidth>500){
+if (document.documentElement.clientWidth > 500) {
   window.alert('请使用手机打开本页面，以保证显示效果');
-  const img=document.createElement('img');
-  img.src='../二维码.png';
-  img.style.position='fixed';
-  img.style.left='50%';
-  img.style.top='50%';
-  img.style.transform='translate(-50%,-50%)';
-  img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)';
+  const img = document.createElement('img');
+  img.src = '../qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
   document.body.appendChild(img)
 }
