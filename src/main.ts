f2f6cb60d563604853a7e98ba97d2e0eb6c16e93
wwf <865475679@qq.com>
2020-03-21 11:54:23 +0800

    添加二维码，保证用户可以用手机打开

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,15 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app');
+
+if(document.documentElement.clientWidth>500){
+  window.alert('请使用手机打开本页面，以保证显示效果');
+  const img=document.createElement('img');
+  img.src='../二维码.png';
+  img.style.position='fixed';
+  img.style.left='50%';
+  img.style.top='50%';
+  img.style.transform='translate(-50%,-50%)';
+  img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img)
+}
